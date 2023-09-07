fc39ec4697c2c0c68e10d63338e654e840591875
Bao Tran <Baottran21@outlook.com>
2023-09-07 13:36:53 -0500

    Got scroll functionality of the webpage

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,25 @@
-const Header = () => {
+import { Link } from "react-scroll";
+
+const NavLinks = [
+  { id: 1, link: "landing-page-container", title: "Home" },
+  { id: 2, link: "about-container", title: "About" },
+];
+
+export default function Header() {
   return (
     <>
-      <div
-        id="header-container"
-        className=" w-full h-[10vh] flex justify-center items-center"
-      >
-        <h1>ByteByBao.Dev</h1>
-      </div>
+      <header className="w-full flex justify-between items-center h-[10vh] bg-[#eee] pl-[5vh] pr-[15vh] sticky top-0 ">
+        ByteByBao.Dev
+        <ul className=" w-[20vw] flex justify-between">
+          {NavLinks.map((elem, index) => (
+            <li key={index} className="cursor-pointer">
+              <Link to={elem.link} smooth={true} offset={-100} duration={500}>
+                {elem.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </header>
     </>
   );
-};
-
-export default Header;
+}
